66c8f0abf1d4682f1b11ea0d5b2e6cd4f174f4b9
Max Moskva <yngmxx@mail.ru>
2021-05-21 16:44:36 +0300

    T1

--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -1,14 +1,13 @@
 import org.junit.Test;
 
 public class MainClassTest extends MainClass{
-    @Test
-    public void testGetLocalNumber()
+    public String testGetLocalNumber()
     {
         int a = this.getLocalNumber();
         if (a==14){
-            System.out.println("Right is it 14");
+            return "Right is it 14";
         } else {
-            System.out.println("Wrong it it not 14");
+            return "Wrong it it not 14";
         }
     }
 }
---
+++ src/Test1.java
@@ -0,0 +1,9 @@
+import org.junit.Test;
+
+public class Test1 extends MainClassTest {
+    @Test
+    public void test1()
+    {
+        System.out.println(this.testGetLocalNumber());
+    }
+}
