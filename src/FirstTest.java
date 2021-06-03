081a92c1cc6deae956f7639c6cfbc97cdc399092
Max Moskva <yngmxx@mail.ru>
2021-06-03 18:21:50 +0300

    III. Первые тесты на Android -до рефакторинга

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -3,7 +3,12 @@ import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.URL;
 
@@ -21,7 +26,7 @@ public class FirstTest {
         capabilities.setCapability("automationName","Appium");
         capabilities.setCapability("appPackage","org.wikipedia");
         capabilities.setCapability("appActivity",".main.MainActivity");
-        capabilities.setCapability("app","/Users/Maxx/Documents/QA_Automation/apks/org.wikipedia_50355_apps.evozi.com.apk");
+        capabilities.setCapability("app","/Users/Maxx/Documents/QA_Automation/apks/org.wikipedia.apk");
 
         driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
     }
@@ -33,6 +38,31 @@ public class FirstTest {
 
     @Test
     public void firstTest(){
-        System.out.println("First test run");
+
+
+        WebElement element_init = waitFor("//*[contains(@text, 'Search Wikipedia')]", "Can't find: Search Wikipedia", 5);
+
+                //driver.findElementByXPath("//*[contains(@text, 'Search Wikipedia')]");
+        element_init.click();
+
+        WebElement element_search = waitFor("//*[contains(@text, 'Search…')]", "Can't find: Search");
+
+                //driver.findElementByXPath("//*[contains(@text, 'Search…')]");
+        element_search.sendKeys("Java");
+
+        waitFor("//*[@resource-id='org.wikipedia:id/page_list_item_container']//*[@text='Object-oriented programming language']", "Can't find: Object-oriented programming language", 15);
+        //System.out.println("First test run");
+    }
+
+    private WebElement waitFor (String xPath, String err_msg, long timeOut){
+        WebDriverWait wait = new WebDriverWait(driver, timeOut);
+        wait.withMessage(err_msg + "\n");
+        By by = By.xpath(xPath);
+        return wait.until(ExpectedConditions.presenceOfElementLocated(by));
+    }
+
+    private WebElement waitFor (String xPath, String err_msg){
+
+        return waitFor(xPath, err_msg, 5);
     }
 }
